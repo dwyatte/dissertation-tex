207176e237d515cd994c31006062e05c8457ae9a
Dean Wyatte <dean.wyatte@colorado.edu>
2014-05-11 10:07:41 -0600

    script robustness

--- bib_histogram.sh
+++ bib_histogram.sh
@@ -1,6 +1,8 @@
+#/bin/zsh
+
 if [ $1 ]
 then # parse years of all refs from bbl file
-    grep \([1-2][0-9][0-9][0-9]\) $1 | awk '{print $NF}' | tr "(" " " | tr ")." " "
+    grep \([1-2][0-9][0-9][0-9]\) $1 | cut -d '(' -f2 | cut -d ')' -f1
 else
     echo "Usage: bib_histogram.sh [file.bbl]"
 fi
